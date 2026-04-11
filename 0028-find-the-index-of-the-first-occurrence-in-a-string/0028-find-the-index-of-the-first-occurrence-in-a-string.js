3563d3b8522cd406d9e70dd6c7e2e8587828d339
Sharika R <sharikar019@gmail.com>
2026-04-11 16:43:20 +0530

    Time: 0 ms (100%), Space: 53.3 MB (85.07%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+   
+    if (needle === "") return 0;
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let match = true;
+
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                match = false;
+                break;
+            }
+        }
+
+        if (match) return i;
+    }
+
+    return -1;
+
+
+};
