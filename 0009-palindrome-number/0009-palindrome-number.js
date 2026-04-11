65b5ad2c0700f7bf4f8afe160b42c9cdc4d60ef5
Sharika R <sharikar019@gmail.com>
2026-04-11 16:39:27 +0530

    Time: 6 ms (66.07%), Space: 64.8 MB (39.5%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = x.toString()
+    const reversed = str.split("").reverse().join("");
+    return str ===reversed;
+    
+};
