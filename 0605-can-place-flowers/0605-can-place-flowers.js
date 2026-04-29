337d91d46a6a114ff90cb03036183ee232f92def
Sharika R <sharikar019@gmail.com>
2026-04-29 09:36:00 +0530

    Time: 1 ms (86.79%), Space: 55.9 MB (72.1%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+if(n===0) return true
+for(let i =0;i<flowerbed.length;i++){
+    if(
+        flowerbed[i] === 0 &&
+        (i===0 || flowerbed[i-1]===0) &&
+        (i===flowerbed.length-1 || flowerbed[i+1] ===0)
+    ){
+        n--
+        i++
+        if(n===0) return true
+    }
+}
+return false
+
+}
+
