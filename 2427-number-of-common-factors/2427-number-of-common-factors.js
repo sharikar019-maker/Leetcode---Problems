5f43fd0d4bea68cba18b577caa15fde3f28f9be1
Sharika R <sharikar019@gmail.com>
2026-05-01 13:40:55 +0530

    Time: 0 ms (100%), Space: 53.7 MB (68.34%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+    let count = 0;
+    let limit = Math.min(a,b)
+    for (let i =1; i<=limit;i++){
+     if(a%i===0 && b%i===0){
+        count ++
+     }
+    }
+    return count
+};
