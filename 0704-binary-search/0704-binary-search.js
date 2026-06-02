82a15a8c76792de1c1cdd1246ebe97243e6d0661
Sharika R <sharikar019@gmail.com>
2026-06-02 10:27:55 +0530

    Time: 0 ms (100%), Space: 59 MB (9.6%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i <= j) {
+        let mid = Math.floor((i + j) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            i = mid + 1;
+        } else {
+            j = mid - 1;
+        }
+    }
+
+    return -1;
+};
