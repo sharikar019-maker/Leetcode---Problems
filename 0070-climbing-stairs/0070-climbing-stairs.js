38e6bcba7329f7d3e063a411f99c16ecd7635c81
Sharika R <sharikar019@gmail.com>
2026-06-15 10:49:03 +0530

    Time: 0 ms (100%), Space: 53.7 MB (36.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,17 +2,15 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    if (n <= 2) return n;
+var climbStairs = function(n){
+    if(n<=2) return n
+    let first = 1
+    let second = 2
+    for(let i=3;i<=n;i++){
+        let current = first + second
+         first = second
+         second = current
 
-    let first = 1;
-    let second = 2;
-
-    for (let i = 3; i <= n; i++) {
-        let current = first + second;
-        first = second;
-        second = current;
     }
-
-    return second;
-};
+    return second
+}
