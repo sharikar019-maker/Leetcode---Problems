1e9c322a1088502765ec145438ab779b5708e745
Sharika R <sharikar019@gmail.com>
2026-08-17 10:15:19 +0530

    Time: 33 ms (99.14%), Space: 54.1 MB (52.73%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function(n) {
+    var result = 0;
+    var count = 32;
+
+    while (count--) {
+        result *= 2;
+        result += n & 1;
+        n = n >> 1;
+    }
+
+    return result;
+};
